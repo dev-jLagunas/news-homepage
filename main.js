2767c80bd38582b80c9cec6c9012d26014721e0e
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-13 12:18:20 +0900

    Complete mobile nav sidebar logic and display styles

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// Import Statements
 import "./CSS/style.css";
+
+//Access DOM elements
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const mobileNav = document.getElementById("nav-links-sm-container");
+const closeBtn = document.querySelector(".close-btn");
+
+// Event Listeners
+// Event Listeners
+hamburgerBtn.addEventListener("click", function () {
+  mobileNav.classList.add("open");
+  overlay.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  mobileNav.classList.remove("open");
+  overlay.style.display = "none";
+});
